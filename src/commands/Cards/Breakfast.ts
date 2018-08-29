d908041b125879a4d9c072a250d68b0bb373b722
spyip <ping.yip@hotmail.com>
2018-08-28 22:02:00 -0700

    Add new Adaptive Cards (#2)

---
+++ src/commands/Cards/Breakfast.ts
@@ -0,0 +1,72 @@
+export default () => ({
+    "$schema": "https://microsoft.github.io/AdaptiveCards/schemas/adaptive-card.json",
+    "version": "1.0",
+    "type": "AdaptiveCard",
+    "body": [
+        {
+            "type": "Container",
+            "items": [
+                {
+                    "type": "TextBlock",
+                    "size": "Medium",
+                    "weight": "Lighter",
+                    "text": "Alarm"
+                }
+            ]
+        },
+        {
+            "type": "Container",
+            "items": [
+                {
+                    "type": "TextBlock",
+                    "size": "Large",
+                    "text": "Breakfast",
+                    "wrap": true
+                }
+            ]
+        },
+        {
+            "type": "TextBlock",
+            "size": "Large",
+            "weight": "Bolder",
+            "text": "10:00 AM"
+        },
+        {
+            "type": "Container",
+            "items": [
+                {
+                    "type": "TextBlock",
+                    "weight": "Lighter",
+                    "text": "Snooze for"
+                },
+                {
+                    "type": "Input.ChoiceSet",
+                    "value": "10m",
+                    "placeholder": "Placeholder text",
+                    "choices": [
+                        {
+                            "title": "10 minutes",
+                            "value": "10m"
+                        },
+                        {
+                            "title": "30 minutes",
+                            "value": "30m"
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    "actions": [
+        {
+            "type": "Action.Submit",
+            "title": "Snooze"
+        },
+        {
+            "type": "Action.Submit",
+            "title": "Dismiss"
+        }
+    ],
+    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+    "version": "1.0"
+})
