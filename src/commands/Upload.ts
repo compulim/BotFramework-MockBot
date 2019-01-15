16adced7848a702ec30f8f47610c87bcf932a3bc
William Wong <compulim@hotmail.com>
2019-01-14 16:33:43 -0800

    Add upload Adaptive Card command

--- src/commands/Upload.ts
+++ src/commands/Upload.ts
@@ -1,4 +1,5 @@
 import { Attachment, TurnContext } from 'botbuilder';
+import fetch from 'node-fetch';
 
 const name = 'File upload';
 
@@ -8,16 +9,49 @@ function help() {
   };
 }
 
+async function fetchJSON(url) {
+  const res = await fetch(url);
+
+  if (res.ok) {
+    const text = await res.text();
+
+    return JSON.parse(text);
+  } else {
+    throw new Error(`Server returned ${ res.status }`);
+  }
+}
+
+function isTrustedAttachmentURL(url) {
+  return (
+    /^https:\/\/directline.botframework.com\//i.test(url) ||
+    /^https?:\/\/localhost(:\d+)?\//i.test(url)
+  );
+}
+
+async function echoAttachment({ contentType, contentUrl, name }) {
+  if (contentType === 'application/json' && isTrustedAttachmentURL(contentUrl)) {
+    // We only fetch content from trusted source, so we don't DDoS anyone.
+
+    return {
+      content: await fetchJSON(contentUrl),
+      contentType: 'application/vnd.microsoft.card.adaptive',
+      name
+    };
+  } else {
+    return {
+      contentType: 'application/octet-stream',
+      contentUrl,
+      name
+    };
+  }
+}
+
 async function processor(context: TurnContext, attachments: Attachment[] = []) {
   if (attachments.length) {
     await context.sendActivity({
       text: 'You have uploaded:',
       type: 'message',
-      attachments: attachments.map(({ contentUrl, name }) => ({
-        contentType: 'application/octet-stream',
-        contentUrl,
-        name
-      }))
+      attachments: await Promise.all(attachments.map(echoAttachment))
     });
   } else {
     await context.sendActivity({
