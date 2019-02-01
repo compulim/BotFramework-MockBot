79f650361c57d02927ecc3c88846babccb351553
William Wong <compulim@hotmail.com>
2019-01-31 16:02:58 -0800

    Dump username

--- src/commands/UserID.ts
+++ src/commands/UserID.ts
@@ -4,13 +4,13 @@ const name = 'User ID';
 
 function help() {
   return {
-    'userid': 'Dump the user ID'
+    'userid': 'Dump the user ID and username'
   };
 }
 
 async function processor(context: TurnContext) {
   await context.sendActivity({
-    text: `Your user ID is \`${ context.activity.from.id }\``,
+    text: `Your user ID is \`${ context.activity.from.id }\`.\n\nAnd your user name is "${ context.activity.from.name }".`,
     type: 'message'
   });
 }
