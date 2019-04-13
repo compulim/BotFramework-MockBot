2bb5ce80a7c42c4d218ec9b72e3015b54673a402
William Wong <compulim@hotmail.com>
2019-04-13 16:02:24 -0700

    Add slow network emulation for Markdown

--- src/commands/Slow.ts
+++ src/commands/Slow.ts
@@ -10,35 +10,52 @@ function help() {
 
 async function processor(context: TurnContext, ...args: string[]) {
   const { PUBLIC_URL } = process.env;
-  const arg = args.map(arg => (arg || '').trim()).filter(arg => arg).join(' ');
+  const arg = (args[0] || '').trim();
 
-  switch (arg) {
-    case 'carousel':
-    default:
-      await context.sendActivity({
-        type: 'message',
-        text: 'This command will slowly show 4 images in carousel',
-        attachmentLayout: 'carousel',
-        attachments: [{
-          contentType: 'image/jpg',
-          contentUrl: `${ PUBLIC_URL }assets/surface1.jpg?slow`,
-          name: 'Microsoft Surface Front View'
-        }, {
-          contentType: 'image/jpg',
-          contentUrl: `${ PUBLIC_URL }assets/surface2.jpg?slow`,
-          name: 'Microsoft Surface Back View'
-        }, {
-          contentType: 'image/jpg',
-          contentUrl: `${ PUBLIC_URL }assets/surface3.jpg?slow`,
-          name: 'Microsoft Surface Side Zoom'
-        }, {
-          contentType: 'image/jpg',
-          contentUrl: `${ PUBLIC_URL }assets/surface4.jpg?slow`,
-          name: 'Microsoft Surface Keyboard Zoom'
-        }]
-      });
+  if (arg === 'markdown') {
+    await context.sendActivity([
+      'This Markdown text contains few images with emulated slow network.',
+      '',
+      `![Photo 1](${ PUBLIC_URL }assets/surface1.jpg?slow)`,
+      '',
+      `![Photo 2](${ PUBLIC_URL }assets/surface2.jpg?slow)`,
+      '',
+      `![Photo 3](${ PUBLIC_URL }assets/surface3.jpg?slow)`,
+      '',
+      `![Photo 4](${ PUBLIC_URL }assets/surface4.jpg?slow)`
+    ].join('\n'));
+  } else {
+    const { PUBLIC_URL } = process.env;
+    const arg = args.map(arg => (arg || '').trim()).filter(arg => arg).join(' ');
 
-      break;
+    switch (arg) {
+      case 'carousel':
+      default:
+        await context.sendActivity({
+          type: 'message',
+          text: 'This command will slowly show 4 images in carousel',
+          attachmentLayout: 'carousel',
+          attachments: [{
+            contentType: 'image/jpg',
+            contentUrl: `${ PUBLIC_URL }assets/surface1.jpg?slow`,
+            name: 'Microsoft Surface Front View'
+          }, {
+            contentType: 'image/jpg',
+            contentUrl: `${ PUBLIC_URL }assets/surface2.jpg?slow`,
+            name: 'Microsoft Surface Back View'
+          }, {
+            contentType: 'image/jpg',
+            contentUrl: `${ PUBLIC_URL }assets/surface3.jpg?slow`,
+            name: 'Microsoft Surface Side Zoom'
+          }, {
+            contentType: 'image/jpg',
+            contentUrl: `${ PUBLIC_URL }assets/surface4.jpg?slow`,
+            name: 'Microsoft Surface Keyboard Zoom'
+          }]
+        });
+
+        break;
+    }
   }
 }
 
