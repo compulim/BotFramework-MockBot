4ca379d6796dfdc610c233df2c275b6161606013
William Wong <compulim@hotmail.com>
2019-11-22 02:07:21 -0800

    Use @babel/register and add static HTML

---
+++ .babelrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    '@babel/proposal-object-rest-spread'
+  ],
+  presets: [
+    '@babel/preset-typescript',
+    ['@babel/preset-env', {
+      targets: {
+        node: '10'
+      },
+      modules: 'commonjs'
+    }]
+  ]
+}
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
 // This is the entry point for Azure Web App via IIS and Kudu
 
+require('@babel/register')(require('./.babelrc'));
 require('./lib/index');
