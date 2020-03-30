82491a28e09ff4e6849dd6d7adf32444e569f2f5
William Wong <compulim@hotmail.com>
2020-03-30 13:26:53 -0700

    Add hero card actions

---
+++ src/commands/HeroCardActions.ts
@@ -0,0 +1,63 @@
+import { TurnContext } from 'botbuilder';
+
+const name = 'Hero card actions';
+// Read argument as a single line
+const mode = 'line';
+
+function help() {
+  return {
+    herocardactions: 'Show a hero card with multiple actions'
+  };
+}
+
+async function processor(context: TurnContext) {
+  await context.sendActivity({
+    type: 'message',
+    text: '',
+    attachmentLayout: 'carousel',
+    attachments: [
+      {
+        contentType: 'application/vnd.microsoft.card.hero',
+        content: {
+          buttons: [
+            {
+              title: 'imBack',
+              type: 'imBack',
+              value: 'string'
+            },
+            {
+              title: 'postBack (string)',
+              type: 'postBack',
+              value: 'string'
+            },
+            {
+              title: 'postBack (JSON)',
+              type: 'postBack',
+              value: { value: 'value' }
+            },
+            {
+              displayText: 'displayText',
+              text: 'text',
+              title: 'messageBack (displayText + text + value)',
+              type: 'messageBack',
+              value: { value: 'value' }
+            },
+            {
+              text: 'text',
+              title: 'messageBack (displayText + text)',
+              type: 'messageBack'
+            },
+            {
+              title: 'messageBack (value)',
+              type: 'messageBack',
+              value: { value: 'value' }
+            }
+          ],
+          title: 'Hero Card Actions'
+        }
+      }
+    ]
+  });
+}
+
+export { help, mode, name, processor };
