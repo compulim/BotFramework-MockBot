71462d6d8da5ca54bdf444385c171db55f70ba6a
William Wong <compulim@hotmail.com>
2020-04-23 00:03:19 -0700

    Add 192.168.0/1.*

--- src/index.ts
+++ src/index.ts
@@ -120,6 +120,7 @@ server.get('/health.txt', async (req, res) => {
 function trustedOrigin(origin) {
   return (
     /^https?:\/\/localhost([\/:]|$)/.test(origin)
+    || /^https?:\/\/192\.168\.(0|1)\.\d{1,3}([\/:]|$)/.test(origin)
     || origin === 'null' // This is for file://index.html
 
     // This is for Docker tests, dotless domain
