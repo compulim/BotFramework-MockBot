806148e16ba18debd51cad04591edd8f2ec5b285
William Wong <compulim@hotmail.com>
2020-05-05 15:47:43 -0700

    Update questions in hero card

--- src/commands/HeroCard.ts
+++ src/commands/HeroCard.ts
@@ -14,49 +14,49 @@ function help() {
 async function processor(context: TurnContext, args: string) {
   const { PUBLIC_URL } = process.env;
 
-  switch (args.trim().toLowerCase()) {
-    case 'herocard long title':
-      await context.sendActivity({
-        type: 'message',
-        text: '',
-        attachmentLayout: 'carousel',
-        attachments: [
-          {
-            contentType: 'application/vnd.microsoft.card.hero',
-            content: {
-              buttons: [
-                {
-                  title: 'messageBack Action with no display text',
-                  type: 'messageBack',
-                  value: 'messageBack Button'
-                }
-              ],
-              images: [
-                {
-                  alt: 'Microsoft Surface Alt',
-                  tap: {
-                    type: 'openUrl',
-                    title: 'Tapped it!',
-                    value: `${PUBLIC_URL}testurl1.html`
-                  },
-                  url: `${PUBLIC_URL}assets/surface1.jpg`
-                }
-              ],
-              tap: {
-                type: 'openUrl',
-                title: 'Tapped it!',
-                value: `${PUBLIC_URL}testurl2.html`
-              },
-              subtitle: 'This is the subtitle',
-              title:
-                'This is a HeroCard with a really, really long title that is intended to test the richCardsWrapTitle style option.'
-            }
-          }
-        ]
-      });
-      break;
+  const command = args.trim().toLowerCase();
 
-    case 'herocard qna':
+  if (command === 'herocard long title') {
+    await context.sendActivity({
+      type: 'message',
+      text: '',
+      attachmentLayout: 'carousel',
+      attachments: [
+        {
+          contentType: 'application/vnd.microsoft.card.hero',
+          content: {
+            buttons: [
+              {
+                title: 'messageBack Action with no display text',
+                type: 'messageBack',
+                value: 'messageBack Button'
+              }
+            ],
+            images: [
+              {
+                alt: 'Microsoft Surface Alt',
+                tap: {
+                  type: 'openUrl',
+                  title: 'Tapped it!',
+                  value: `${PUBLIC_URL}testurl1.html`
+                },
+                url: `${PUBLIC_URL}assets/surface1.jpg`
+              }
+            ],
+            tap: {
+              type: 'openUrl',
+              title: 'Tapped it!',
+              value: `${PUBLIC_URL}testurl2.html`
+            },
+            subtitle: 'This is the subtitle',
+            title:
+              'This is a HeroCard with a really, really long title that is intended to test the richCardsWrapTitle style option.'
+          }
+        }
+      ]
+    });
+  } else if (command.startsWith('herocard qna')) {
+    if (command === 'herocard qna' || command.startsWith('herocard qna 1')) {
       await context.sendActivity({
         type: 'message',
         text: 'Where are you from?',
@@ -68,7 +68,7 @@ async function processor(context: TurnContext, args: string) {
                 {
                   title: 'United States',
                   type: 'imBack',
-                  value: 'I am from United States.'
+                  value: 'herocard qna 2 I am from United States.'
                 },
                 {
                   title: 'Hong Kong',
@@ -80,6 +80,9 @@ async function processor(context: TurnContext, args: string) {
           }
         ]
       });
+    }
+
+    if (command === 'herocard qna' || command.startsWith('herocard qna 2')) {
       await context.sendActivity({
         type: 'message',
         text: 'What language do you speak?',
@@ -103,67 +106,66 @@ async function processor(context: TurnContext, args: string) {
           }
         ]
       });
-      break;
-
-    default:
-      await context.sendActivity({
-        type: 'message',
-        text: '',
-        attachmentLayout: 'carousel',
-        attachments: [
-          {
-            contentType: 'application/vnd.microsoft.card.hero',
-            content: {
-              buttons: [
-                {
-                  title: 'imBack Action',
-                  type: 'imBack',
-                  value: 'imBack Button'
-                },
-                {
-                  title: 'postBack Action',
-                  type: 'postBack',
-                  value: 'postBack Button'
-                },
-                {
-                  displayText: 'Send messageBack with display text',
-                  title: 'messageBack Action with displayText',
-                  type: 'messageBack',
-                  value: 'messageBack Button'
-                },
-                {
-                  title: 'messageBack Action with no display text',
-                  type: 'messageBack',
-                  value: 'messageBack Button'
-                }
-              ],
-              images: [
-                {
-                  alt: 'Microsoft Surface Alt',
-                  tap: {
-                    type: 'openUrl',
-                    title: 'Tapped it!',
-                    value: `${PUBLIC_URL}testurl1.html`
-                  },
-                  url: `${PUBLIC_URL}assets/surface1.jpg`
-                }
-              ],
-              tap: {
-                type: 'openUrl',
-                title: 'Tapped it!',
-                value: `${PUBLIC_URL}testurl2.html`
+    }
+  } else {
+    await context.sendActivity({
+      type: 'message',
+      text: '',
+      attachmentLayout: 'carousel',
+      attachments: [
+        {
+          contentType: 'application/vnd.microsoft.card.hero',
+          content: {
+            buttons: [
+              {
+                title: 'imBack Action',
+                type: 'imBack',
+                value: 'imBack Button'
               },
-              subtitle: 'This is the subtitle',
-              text:
-                '**Price: $XXX.XX USD**\r\n------\n Additional details\r\n' +
-                '1. List item 1 \n' +
-                '2. List item 2 \n' +
-                '3. List item 3',
-              title: '[Details about image 1](https://microsoft.com)'
-            }
+              {
+                title: 'postBack Action',
+                type: 'postBack',
+                value: 'postBack Button'
+              },
+              {
+                displayText: 'Send messageBack with display text',
+                title: 'messageBack Action with displayText',
+                type: 'messageBack',
+                value: 'messageBack Button'
+              },
+              {
+                title: 'messageBack Action with no display text',
+                type: 'messageBack',
+                value: 'messageBack Button'
+              }
+            ],
+            images: [
+              {
+                alt: 'Microsoft Surface Alt',
+                tap: {
+                  type: 'openUrl',
+                  title: 'Tapped it!',
+                  value: `${PUBLIC_URL}testurl1.html`
+                },
+                url: `${PUBLIC_URL}assets/surface1.jpg`
+              }
+            ],
+            tap: {
+              type: 'openUrl',
+              title: 'Tapped it!',
+              value: `${PUBLIC_URL}testurl2.html`
+            },
+            subtitle: 'This is the subtitle',
+            text:
+              '**Price: $XXX.XX USD**\r\n------\n Additional details\r\n' +
+              '1. List item 1 \n' +
+              '2. List item 2 \n' +
+              '3. List item 3',
+            title: '[Details about image 1](https://microsoft.com)'
           }
-        ]
-      });
+        }
+      ]
+    });
   }
 }
 
