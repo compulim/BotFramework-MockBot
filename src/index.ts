ca53a83125aa96204241ae283d298a8e40fc74f6
William Wong <compulim@hotmail.com>
2021-03-11 03:30:07 -0800

    Add hawo-acs-dev-appserver.azurewebsites.net

--- src/index.ts
+++ src/index.ts
@@ -150,6 +150,7 @@ function trustedOrigin(origin) {
     /^https:\/\/microsoft\.github\.io(\/|$)/.test(origin) ||
     /^https:\/\/bfxwebchatfullbundle\.azurewebsites\.net(\/|$)/.test(origin) ||
     /^https:\/\/webchattest\.blob\.core\.windows\.net(\/|$)/.test(origin) ||
+    /^https:\/\/hawo-acs-dev-appserver\.azurewebsites\.net(\/|$)/.test(origin) ||
     // This is CodePen
     /^https:\/\/cdpn\.io(\/|$)/.test(origin) ||
     /^https:\/\/s\.codepen\.io(\/|$)/.test(origin)
